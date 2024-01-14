7e2fd67ebaabb36c00bc13100dd14102a4c7065f
Dryusdan <contact@dryusdan.fr>
2024-01-14 20:34:34 +0100

    Fix crash if config.yaml not exist

--- garmin_to_fittrackee/logs.py
+++ garmin_to_fittrackee/logs.py
@@ -1,18 +1,28 @@
 import logging
-import yaml
 from pathlib import Path
+
+import yaml
 from rich.logging import RichHandler
 
 home = str(Path.home())
 config_path = f"{home}/.config/garmin-to-fittrackee"
 
+
 class Log:
     def __new__(self, name: str = "default", level: str = None):
-        with open(f"{config_path}/config.yml", "r") as file:
-            config = yaml.safe_load(file)
-        if config is not None and "log" in config and "level" in config["log"] and level is None:
-            level = config["log"]["level"]
-        else:
+        if Path(f"{config_path}/config.yml").is_file():
+            with open(f"{config_path}/config.yml", "r") as file:
+                config = yaml.safe_load(file)
+            if (
+                config is not None
+                and "log" in config
+                and "level" in config["log"]
+                and level is None
+            ):
+                level = config["log"]["level"]
+            else:
+                level = "INFO"
+        elif level is None:
             level = "INFO"
 
         log = logging.basicConfig(
