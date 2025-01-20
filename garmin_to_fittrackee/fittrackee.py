07e975520addd55d3635ef7fe48a60149b106f05
Dryusdan <contact@dryusdan.fr>
2025-01-20 17:48:07 +0100

    Increase max version

--- garmin_to_fittrackee/fittrackee.py
+++ garmin_to_fittrackee/fittrackee.py
@@ -308,7 +308,7 @@ class Fittrackee:
             "data" in config
             and "version" in config["data"]
             and Version(config["data"]["version"]) >= Version("0.7.29")
-            and Version(config["data"]["version"]) < Version("0.9")
+            and Version(config["data"]["version"]) < Version("0.10")
         ):
             return False
         return True
